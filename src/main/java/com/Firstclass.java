53bcce95c04a5e2c2502cfc875fec241487df4ad
Naresh Reddy <bluebird.460@gmail.com>
2023-02-14 18:21:34 +0530

    comitted

---
+++ src/main/java/com/Firstclass.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.testng.annotations.Test;
+
+public class Firstclass {
+
+	@Test
+	public static void test1() {
+
+		System.out.println("Firt Test");
+	}
+
+	@Test
+	public static void test2() {
+
+		System.out.println("Second Test");
+	}
+
+}
