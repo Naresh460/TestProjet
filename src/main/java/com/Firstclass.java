23a4f5ca686ce6f52229aca2114872cd6902e59f
Naresh Reddy <bluebird.460@gmail.com>
2023-02-14 18:38:49 +0530

    comitted

--- src/main/java/com/Firstclass.java
+++ src/main/java/com/Firstclass.java
@@ -1,13 +1,20 @@
 package com;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Firstclass {
 
 	@Test
 	public static void test1() {
 
-		System.out.println("Firt Test");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println("Title of the page is"+driver.getTitle());
 	}
 
 	@Test
