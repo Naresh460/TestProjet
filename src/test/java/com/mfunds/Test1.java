e4655a85804ee7554a8e264c3648d5d62af40fd0
Naresh Reddy <bluebird.460@gmail.com>
2023-12-13 16:45:07 +0530

    xml updated

---
+++ src/test/java/com/mfunds/Test1.java
@@ -0,0 +1,13 @@
+package com.mfunds;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public static void est1() {
+		System.out.println("*********Test1 class**********");
+		
+	}
+
+}
